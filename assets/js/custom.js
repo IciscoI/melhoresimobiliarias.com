2a06966d52f9420a5fc376db65453ca70fd737bb
IciscoI <131164726+IciscoI@users.noreply.github.com>
2023-04-18 13:20:13 +0100

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
